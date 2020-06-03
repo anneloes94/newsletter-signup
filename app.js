41132d26a14c6ab206c965e6cfb7089cff8f628f
Anneloes <anneloes9405@hotmail.com>
2020-06-03 13:49:57 -0400

    adding mailchimp post request
    Signed-off-by: Anneloes <anneloes9405@hotmail.com>

--- app.js
+++ app.js
@@ -11,6 +11,37 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html")
 })
 
+app.post("/", function(req, res) {
+
+  const firstName = req.body.firstName
+  const lastName = req.body.lastName
+  const emailAddress = req.body.emailAddress  
+
+  const data = {
+    members: [
+      {
+        email_address: emailAddress,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        }
+      }
+    ]
+  }
+
+  const jsonData = JSON.stringify(data)
+
+  const apiKey = process.env.MAILCHIMP_KEY
+  const listID = process.env.LIST_ID
+
+  const url = `https://usX.api.mailchimp.com/3.0/lists/`
+
+  https.request(url, options, (response) => {
+
+  })
+})
+
 app.listen(3000, function(){
   console.log("Server is running on port 3000")
 })
