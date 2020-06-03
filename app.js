1be4fcd9ba8a56e88c5e6698599d8ad8b8699ac5
Anneloes <anneloes9405@hotmail.com>
2020-06-03 15:25:36 -0400

    setup changes
    Signed-off-by: Anneloes <anneloes9405@hotmail.com>

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const bodyParser = require("body-parser")
-require('dotenv').config()
-const express = require("express")
-const request = require("request")
-const https = require("https")
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const express = require("express");
+const request = require("request");
+const https = require("https");
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static(__dirname + "/public"))
+const port = process.env.PORT || 3000;
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/signup.html")
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 
-app.post("/", function(req, res) {
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
 
-  const firstName = req.body.firstName
-  const lastName = req.body.lastName
-  const emailAddress = req.body.emailAddress
-  
+app.post("/", function (req, res) {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const emailAddress = req.body.emailAddress;
+
   const data = {
     members: [
       {
@@ -27,40 +28,38 @@ app.post("/", function(req, res) {
         merge_fields: {
           FNAME: firstName,
           LNAME: lastName,
-        }
-      }
-    ]
-  }
+        },
+      },
+    ],
+  };
 
-  const jsonData = JSON.stringify(data)
+  const jsonData = JSON.stringify(data);
 
-  const apiKey = process.env.MAILCHIMP_KEY
-  const listID = process.env.LIST_ID
+  const apiKey = process.env.MAILCHIMP_KEY;
+  const listID = process.env.LIST_ID;
 
-  const url = `https://us10.api.mailchimp.com/3.0/lists/${listID}`
-  
+  const url = `https://us10.api.mailchimp.com/3.0/lists/${listID}`;
+
   const options = {
     method: "POST",
-    auth: `anneloes94:${apiKey}`
-  }
+    auth: `anneloes94:${apiKey}`,
+  };
 
   const request = https.request(url, options, (response) => {
-    
     if (response.statusCode === 200) {
-      res.sendfile(__dirname + "/success.html")
+      res.sendfile(__dirname + "/success.html");
     } else {
-      res.sendfile(__dirname + "/failure.html")
+      res.sendfile(__dirname + "/failure.html");
     }
     response.on("data", (data) => {
-      console.log(JSON.parse(data))
+      console.log(JSON.parse(data));
+    });
+  });
 
-    })
-  })
+  request.write(jsonData);
+  request.end();
+});
 
-  request.write(jsonData)
-  request.end()
-})
-
-app.listen(3000, function(){
-  console.log("Server is running on port 3000")
-})
+app.listen(port, function () {
+  console.log("Server is running on port 3000");
+});
